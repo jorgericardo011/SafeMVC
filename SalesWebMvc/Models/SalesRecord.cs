b60e80efe422f37954779df4b7fc654d17ebe1fc
Estação_Jorge <ricardo10caldas@gmail.com>
2022-03-13 00:43:54 -0300

    Other Entities for Project

---
+++ SalesWebMvc/Models/SalesRecord.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMvc.Models
+{
+    public class SalesRecord
+    {
+        public int Id { get; set; }
+        public DateTime Data { get; set; }
+        public double Amount { get; set; }
+        public SalesStatus Status { get; set; }
+
+        public Seller Seller { get; set; }
+
+     
+        public SalesRecord(int id, DateTime data, double amount, SalesStatus status)
+        {
+            Id = id;
+            Data = data;
+            Amount = amount;
+            Status = status;
+        }
+    }
+}
